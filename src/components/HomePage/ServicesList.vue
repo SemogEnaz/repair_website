097eda9456f14d673e4fe83b207d161b894d52b9
Zane Gomes <semogenaz@gmail.com>
2026-04-23 09:38:06 +1000

    Fixed css of services

--- src/components/HomePage/ServicesList.vue
+++ src/components/HomePage/ServicesList.vue
@@ -1,13 +1,35 @@
 <template>
-    <section class="services">
-      <div class="card">
+    <section class="flex justify-around gap-4">
+      <div class="card bg-blue-950">
         <h3>🏪 Visit Us</h3>
         <p>Quick repairs while you wait. Most fixes done in under 45 minutes.</p>
       </div>
 
-      <div class="card">
+      <div class="card dark-blue">
         <h3>🚗 We Come to You</h3>
         <p>Home, uni, or work. Same-day mobile service.</p>
       </div>
     </section>
 </template>
+
+<style scoped>
+
+section {
+  background: #111;
+  border-radius: 8px;
+  padding: 30px;
+  margin-top: 60px;
+}
+
+.card {
+  width: 50%;
+  border-radius: 8px;
+  padding: 20px;
+  text-align: center;
+}
+
+h3 {
+  font-size: 1.5rem;
+  margin-bottom: 5px;
+}
+</style>
