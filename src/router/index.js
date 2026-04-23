ac064e3bcf7b83ba6f41701c54584b82b4188eb3
Zane Gomes <semogenaz@gmail.com>
2026-04-23 10:55:29 +1000

    Added some new pages for the nav bar

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,10 @@ import HomePage from '@/views/HomePage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/', name: 'home', component: HomePage
-    },
+    { path: '/', name: 'home', component: HomePage },
+    { path: '/calculator', name: 'calculator', component: () => import('@/views/CalculatorView.vue') },
+    { path: '/about', name: 'about', component: () => import('@/views/AboutView.vue') },
+    { path: '/contact', name: 'contact', component: () => import('@/views/ContactView.vue') },
   ],
 })
 
