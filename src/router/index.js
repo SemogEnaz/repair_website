9f8ff33553568518325f6ac9efb828ec1c637cfb
Zane Gomes <semogenaz@gmail.com>
2026-05-04 00:55:50 +1000

    Removed outcall view and route

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,6 @@ const router = createRouter({
     { path: '/about', name: 'about', component: () => import('@/views/AboutView.vue') },
     { path: '/sell', name: 'sell', component: () => import('@/views/SellView.vue') },
     { path: '/buy', name: 'buy', component: () => import('@/views/BuyView.vue') },
-    { path: '/outcall', name: 'outcall', component: () => import('@/views/OutcallView.vue') },
   ],
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) return { el: to.hash, behavior: 'smooth' }
