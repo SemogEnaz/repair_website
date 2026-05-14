601cd896e96e57fdb58cf38b62fac275770226b0
Zane Gomes <semogenaz@gmail.com>
2026-05-14 10:04:56 +1000

    fixed import errors after moving the component files

--- src/components/HomePage/Calculator.vue
+++ src/components/HomePage/Calculator.vue
@@ -117,7 +117,7 @@
 
 <script setup>
 import { ref, computed, onMounted } from 'vue'
-import Alert from './Alert.vue'
+import Alert from '../Alert.vue'
 
 // ---------------- DATA ----------------
 
