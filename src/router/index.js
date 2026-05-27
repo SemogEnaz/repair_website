05be95310873c813bb82fb5629b8d7593279518e
Zane Gomes <semogenaz@gmail.com>
2026-05-27 11:32:40 +1000

    Added start scroll back at top when routerlink is changed

--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,18 @@ const router = createRouter({
     { path: '/', name: 'home', component: HomePage },
     { path: '/about', name: 'about', component: () => import('@/views/AboutView.vue') },
     { path: '/buy', name: 'buy', component: () => import('@/views/BuyView.vue') },
+    { path: '/battery', name: 'battery', component: () => import('@/views/WhyBatteryRepairView.vue') },
   ],
   scrollBehavior(to, from, savedPosition) {
+
+	// Smooth scroll when clicking buttons that take a user somewhere within the page
     if (to.hash) return { el: to.hash, behavior: 'smooth' }
+
+	// If the user switches to another router view (page) they continue where they left off
+	if (savedPosition) return savedPosition;
+
+	// Otherwise start at the top
+	return { top: 0 }
   }
 })
 
